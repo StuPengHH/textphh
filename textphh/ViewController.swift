e82f016441354de77351814b1e805fd9b0127b47
ciecS20171106331 <565738655@qq.com>
2018-10-13 10:16:51 +0800

    plus
    come on

--- textphh/ViewController.swift
+++ textphh/ViewController.swift
@@ -9,7 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var clmx: UITextField!
+    @IBOutlet weak var clmy: UITextField!
+    @IBOutlet weak var clmz: UITextField!
+    @IBAction func caculator(_ sender: Any) {
+        var a=0
+        a=Int(clmx.text!)!
+        var b=0
+        b=Int(clmy.text!)!
+        var c:Int=0
+        c=a+b
+        clmz.text="\(c)"
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
